67e36aab2c769d4523bb7ceb6efc8b9afb69f4a4
Matthew Rothenberg <mrothenberg@gmail.com>
2019-05-12 17:00:29 -0400

    fix: reverse-chronological commits in changelogs

--- git.go
+++ git.go
@@ -31,11 +31,31 @@ func getLatestRelease(owner, repo string) (*github.RepositoryRelease, error) {
 //
 // It will automatically use an OAuth scoped token if GITHUB_RELEASE environment
 // variable is set, or an unauthed client otherwise.
+//
+// Unlike git log base..head, which returns commits in reverse chronlogical
+// order, the GitHub V3 API returns in chronological order. To do what most
+// changelog generators do and show commits in reverse chronological, we just
+// mutate the actual CommitsComparison.Commits subfield prior to returning it
+// here.
+//
+// Interestingly enough, this is the exact opposite of what is claimed in the
+// GitHub API documentation, which says log is chronological, see:
+// https://developer.github.com/v3/repos/commits/#compare-two-commits.
 func compareRelease(owner, repo, tagName string) (*github.CommitsComparison, error) {
 	client, ctx := defaultGithubClient(), context.Background()
 	defer timeTrack(time.Now(), "client.Repositories.CompareCommits()")
 	cc, _, err := client.Repositories.CompareCommits(ctx, owner, repo, tagName, "HEAD")
+	if cc != nil {
+		reverseCommitOrder(cc)
+	}
 	return cc, err
+}
+
+func reverseCommitOrder(cc *github.CommitsComparison) {
+	for i := len(cc.Commits)/2 - 1; i >= 0; i-- {
+		opp := len(cc.Commits) - 1 - i
+		cc.Commits[i], cc.Commits[opp] = cc.Commits[opp], cc.Commits[i]
+	}
 }
 
 // defaultGithubClient returns a OAuth scoped Github API Client if GITHUB_TOKEN
